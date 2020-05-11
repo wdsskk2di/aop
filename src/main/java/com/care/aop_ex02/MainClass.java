e2c25b24b71fe87ccb529df381821c9314eb63c3
123 <123@123>
2020-05-11 17:19:37 +0900

    appAOP_01 aop 지정 할당, appAOP_2는 anot을 이용한 aop 자동 할당

--- src/main/java/com/care/aop_ex02/MainClass.java
+++ src/main/java/com/care/aop_ex02/MainClass.java
@@ -9,7 +9,8 @@ public class MainClass {
 		CoreClass tc = ctx.getBean("core",CoreClass.class);
 		
 		System.out.println("MainClass start");
-		tc.corePrint();
+		tc.corePrint();//corePrint()실행 -> aop:around로 CommonClass의 commonAOP()실행. 
+						// CommonClass의 beforeAdvice() 실행조건인 test() 메소드는 부르지 않았으므로 실행x
 		System.out.println("MainClass end");
 		ctx.close();
 	}
