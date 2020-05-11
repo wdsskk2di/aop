2c8d5fd65a146ddaff1827ca8b8478c273d5fbf0
123 <123@123>
2020-05-11 17:44:24 +0900

    주석 정리

--- src/main/java/com/care/aop_ex02/CommonClass.java
+++ src/main/java/com/care/aop_ex02/CommonClass.java
@@ -1,14 +1,17 @@
 package com.care.aop_ex02;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
-@Aspect //공통기능으로 쓰겠다는 어노테이션
+@Aspect //공통기능으로 쓰겠다는 어노테이션. applicationAOP_02.xml의 <aop:aspectj-autoproxy/>에서 자동으로 찾음
 public class CommonClass {//공통 기능
-	@Around("within(com.care.aop_ex02.*)")  //around로 쓸거란걸 알려주는 어노테이션
-	public void commonAOP(ProceedingJoinPoint joinpoint) {	//"within(com.care.aop_ex02.*)" == CoreClass안의 어느 메소드가 불려지든 그 전후에 호출된다
+	
+	//around로 쓸거란걸 알려주는 어노테이션
+	@Around("within(com.care.aop_ex02.*)")	//"within(com.care.aop_ex02.*)" == CoreClass안의 어느 메소드가 불려지든 그 전후에 호출된다
+	public void commonAOP(ProceedingJoinPoint joinpoint) {
 		try {
 			System.out.println("===공통 기능 시작===");
 			joinpoint.proceed();	//일시중지. CoreClass실행
@@ -18,8 +21,9 @@ public class CommonClass {//공통 기능
 		}
 	}
 	
-	@Before("execution(*com.care.aop_ex02.CoreClass.test()") //before로 쓸거란걸 알려주는 어노테이션
-	public void beforeAdvice(ProceedingJoinPoint joinpoint) { //"execution(*com.care.aop_ex02.CoreClass.test()" == CoreClass의 test()가 불려지면 test()실행 전에 호출됨
+	//before로 쓸거란걸 알려주는 어노테이션
+	@Before("execution(* com.care.aop_ex02.CoreClass.test())") //"execution(*com.care.aop_ex02.CoreClass.test()" == CoreClass의 test()가 불려지면 test()실행 전에 호출됨
+	public void beforeAdvice(JoinPoint joinpoint) {
 		System.out.println("test() 핵심 기능 전에 실행"); 	
 	}
 }
--- src/main/java/com/care/aop_ex02/MainClass.java
+++ src/main/java/com/care/aop_ex02/MainClass.java
@@ -9,8 +9,12 @@ public class MainClass {
 		CoreClass tc = ctx.getBean("core",CoreClass.class);
 		
 		System.out.println("MainClass start");
-		tc.corePrint();//corePrint()실행 -> aop:around로 CommonClass의 commonAOP()실행. 
-						// CommonClass의 beforeAdvice() 실행조건인 test() 메소드는 부르지 않았으므로 실행x
+		tc.corePrint(); //corePrint()실행 -> aop:around로 CommonClass의 commonAOP()실행. 
+						// CommonClass의 beforeAdvice() 실행조건인 test() 메소드는 부르지 않았으므로 실행x		
+		tc.corePrint02();
+		
+						//aop:around로 tes()메소드 실행 전, 후로  CommonClass의 commonAOP()실행(CoreClass안의 어느 메소드가 불려지든 그 전후에 호출되게되어 있다). 
+		tc.test();  	// CommonClass의 beforeAdvice() 실행조건인 test() 메소드 호출. test()실행 전에 beforeAdvice() 실행.
 		System.out.println("MainClass end");
 		ctx.close();
 	}
